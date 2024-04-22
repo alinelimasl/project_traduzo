0739220edb674f240f14cd9ed6e5edacd0bb55e2
Aline <aline_lima@outlook.com>
2024-04-22 13:33:16 -0300

    Correção requisito 6

--- src/controllers/language_controller.py
+++ src/controllers/language_controller.py
@@ -20,22 +20,23 @@ def translate_index():
 
 
 @language_controller.route("/", methods=["POST"])
-def translate_post():
-    text_to_translate = request.form.get("text_to_translate")
-    translate_from = request.form.get("translate_from")
-    translate_to = request.form.get("translate_to")
+def post_translate():
+    text_to_translate = request.form.get("text-to-translate")
+    translate_from = request.form.get("translate-from")
+    translate_to = request.form.get("translate-to")
 
-    translator = GoogleTranslator(
-        source=translate_from, target_language=translate_to
+    translated = GoogleTranslator(
+        source=translate_from, target=translate_to
     ).translate(text_to_translate)
 
-    languages = LanguageModel.list_dicts()
     phrases = {
-        "languages": languages,
         "text_to_translate": text_to_translate,
         "translate_from": translate_from,
         "translate_to": translate_to,
-        "translated": translator,
+        "translated": translated,
     }
 
-    return render_template("index.html", **phrases)
+    languages = LanguageModel.list_dicts()
+    return render_template(
+        "index.html", languages=languages, **phrases
+    )
